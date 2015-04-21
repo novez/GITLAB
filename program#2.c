ac377ff8e1062d8f72d61c2b76acb6961a8cdd37
novez <novez_1@hotmail.com>
2015-04-21 18:31:25 +0700

    First version

---
+++ program#2.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/* Calculate a students grade.
+* Ask the user for quizzes, midterm and final exam score,
+*then print user results. Quizzes count 25%, midterm 25%,
+*final exam 50%. Final grade is a number, not a letter.
+*/
+int main()
+	{
+	char stringInput[128];
+	int totalScores = 0;
+	int quizScore = 0;
+	int midScore = 0;
+	int finalScore = 0;
+	int averageScore = 0;
+	int grade = 0;
+	int quiz;
+	memset(stringInput,0,sizeof(stringInput));
+
+	/* Calculate average of quizzes */
+	for (quiz = 1; quiz <=7; quiz++)
+		{
+		printf("Enter score for quiz number %d: ", quiz);
+		fgets(stringInput,128, stdin);
+		sscanf(stringInput,"%d",&quizScore);
+		totalScores = totalScores + quizScore;
+		}
+	averageScore = totalScores/7;
+
+	/* Get the midterm exam score */
+	printf("Enter midterm exam score: ");
+	fgets(stringInput,128, stdin);
+	sscanf(stringInput,"%d",&midScore);
+
+	/* Get the final exam score */
+	printf("Enter final exam score: ");
+	fgets(stringInput,128, stdin);
+	sscanf(stringInput,"%d",&finalScore);
+
+	/* Calculate grade */
+	grade = averageScore * 0.25 + midScore * 0.25 + finalScore * 0.5;
+	printf("Your grade is %d\n",grade);
+	if (grade < 50)
+	printf("Sorry, you've failed the course.\n");
+else
+	printf("Congratulation! You passed the course.\n");
+exit(0);
+}
